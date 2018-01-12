94313066ba35fbcd860f6c3cad492d0dd7c460eb
xuxuan <xuxuan819@163.com>
2018-01-12 14:57:24 +0800

    commit

--- springInActionChapter02/src/main/java/springInActionChapter02/App.java
+++ springInActionChapter02/src/main/java/springInActionChapter02/App.java
@@ -1,13 +1,41 @@
 package springInActionChapter02;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World!");
+
+		Class<Demo> c = Demo.class;
+		// 获取需要处理的方法Method实例
+		Method method = c.getMethod("print", new Class[] {});
+		if (method.isAnnotationPresent(TestAnnotation.class)) {
+			
+		}
+	}
+
+	class Demo {
+		@TestAnnotation("hhee")
+		public void print() {
+
+		}
+	}
+
+	@Retention(value = RetentionPolicy.RUNTIME)
+	@interface TestAnnotation {
+
+		String name() default "";
+
+		String[] value() default {};
+	}
 }
--- springMvc/src/main/java/spittr/web/HomeController.java
+++ springMvc/src/main/java/spittr/web/HomeController.java
@@ -1,15 +1,28 @@
 package spittr.web;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*; 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import spittr.data.SpittleRepository;
+
 @Controller
+@RequestMapping("/spittles")
 public class HomeController {
 
-	@RequestMapping(value="/abc", method=GET)
-	public String home() {
-		System.out.println("123455");
-		return "home";
+	private SpittleRepository spittleRepository;
+	
+	@Autowired
+	public HomeController(SpittleRepository spittleRepository) {
+		this.spittleRepository=spittleRepository;
+	}
+	
+	@RequestMapping(method=GET)
+	public String spittles(Model model) {
+		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+		return "spittles";
 	}
 }
